3800e5f7fe36884f80219ae770fc82ac38b462ab
Slava <slava91petrushin@yandex.ru>
2022-09-24 21:44:44 +0300

    fix validator

--- src/validators/blogsValidator.ts
+++ src/validators/blogsValidator.ts
@@ -6,7 +6,7 @@ function checkUrl(url: string){
 }
 
 export const createAndUpdateBlogValidator = [
-	body("name").isString().isLength({max: 15}).withMessage("Укажите имя"),
-	body("youtubeUrl").isString().isLength({max: 100}).custom(checkUrl).withMessage("Укажите валидную ссылку"),
+	body("name").isString().trim().isLength({min: 1, max: 15}).withMessage("Укажите имя"),
+	body("youtubeUrl").isString().trim().isLength({max: 100}).custom(checkUrl).withMessage("Укажите валидную ссылку"),
 ];
 
--- src/validators/postsValidator.ts
+++ src/validators/postsValidator.ts
@@ -4,5 +4,5 @@ export const createAndUpdatePostsValidator = [
 	body("title").isString().isLength({min: 1, max: 30}).withMessage("Укажите заголовок"),
 	body("shortDescription").isString().isLength({min: 1, max: 100}).withMessage("Укажите краткое описание"),
 	body("content").isString().isLength({min: 1, max: 1000}).withMessage("Напишите пост"),
-	body("blogId").isString().withMessage("Укажите ID блога"),
+	body("blogId").isString().trim().withMessage("Укажите ID блога"),
 ];
